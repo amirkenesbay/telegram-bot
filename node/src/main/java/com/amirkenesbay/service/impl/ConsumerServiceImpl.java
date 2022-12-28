5505beb4d00afcfc5f43b6b9a04c0cd129906388
amirkenesbay <omirbek.kenesbay@samgau.com>
2022-12-29 01:23:38 +0600

    Added mainservice for sending a message to the answer queue and for sending data to the database

--- node/src/main/java/com/amirkenesbay/service/impl/ConsumerServiceImpl.java
+++ node/src/main/java/com/amirkenesbay/service/impl/ConsumerServiceImpl.java
@@ -1,11 +1,10 @@
 package com.amirkenesbay.service.impl;
 
 import com.amirkenesbay.service.ConsumeService;
-import com.amirkenesbay.service.ProduceService;
+import com.amirkenesbay.service.MainService;
 import lombok.extern.log4j.Log4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import static com.amirkenesbay.model.RabbitQueue.*;
@@ -13,22 +12,17 @@ import static com.amirkenesbay.model.RabbitQueue.*;
 @Service
 @Log4j
 public class ConsumerServiceImpl implements ConsumeService {
-    private final ProduceService produceService;
+    private final MainService mainService;
 
-    public ConsumerServiceImpl(ProduceService produceService) {
-        this.produceService = produceService;
+    public ConsumerServiceImpl(MainService mainService) {
+        this.mainService = mainService;
     }
 
     @Override
     @RabbitListener(queues = TEXT_MESSAGE_UPDATE)
     public void consumeTextMessageUpdates(Update update) {
         log.debug("NODE: TEXT MESSAGE IS RECEIVED");
-
-        var message = update.getMessage();
-        var sendMessage = new SendMessage();
-        sendMessage.setChatId(message.getChatId().toString());
-        sendMessage.setText("Hello from NODE");
-        produceService.produceAnswer(sendMessage);
+        mainService.processTextMessage(update);
     }
 
     @Override
